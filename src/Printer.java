5103ce4d5c22a9facd2f23fa09872248ce6efb46
Vlada <80097541+Rafaelkaaa@users.noreply.github.com>
2022-09-21 15:56:35 +0300

    Redacted Printer class

--- src/Printer.java
+++ src/Printer.java
@@ -1,17 +1,5 @@
 public class Printer {
-    public static void main(String[] args) {
-
-        Shape circle = new Circle();
-        Shape oval = new Oval();
-        Shape hexagon = new Hexagon();
-        Shape pentagon = new Pentagon();
-        Shape quad = new Quad();
-
-        circle.printFigureName();
-        oval.printFigureName();
-        hexagon.printFigureName();
-        pentagon.printFigureName();
-        quad.printFigureName();
-
+    public static void printShape(Shape shape){
+        shape.printFigureName();
     }
 }
